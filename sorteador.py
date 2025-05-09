d07cc41a65bec93fc24db7d8f4daf9d53e6eabf5
Nathalia N Silva <82617332+nathndss@users.noreply.github.com>
2025-05-09 15:22:14 -0300

    Update sorteador.py

--- sorteador.py
+++ sorteador.py
@@ -36,7 +36,7 @@ st.markdown("<h1>🎉 Sorteador Online 🎉</h1>", unsafe_allow_html=True)
 
 # Entrada de dados
 st.markdown("<h3 style='color: white;'>✍️ Digite os nomes ou números separados por vírgula:</h3>", unsafe_allow_html=True)
-entrada = st.text_area(label="", height=150)
+entrada = st.text_area(label="", height=140)
 
 # Botão de sorteio
 col1, col2, col3 = st.columns([1, 2, 1])  # Cria 3 colunas, a do meio é maior
