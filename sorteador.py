7c3fa509ed881f1963b0bf2d7f4a994464097001
Nathalia N Silva <82617332+nathndss@users.noreply.github.com>
2025-05-09 13:41:52 -0300

    Update sorteador.py

--- sorteador.py
+++ sorteador.py
@@ -22,9 +22,9 @@ st.markdown("""
     .stButton button {
         background-color: #6a0dad;
         color: white;
-        font-size: 16px;
-        border-radius: 8px;
-        padding: 8px 20px;
+        font-size: 25px;
+        border-radius: 16px;
+        padding: 16px 24px;
     }
     </style>
 """, unsafe_allow_html=True)
